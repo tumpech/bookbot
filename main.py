013e2f28ac34eff96817032e151408b2ee994183
tumpech <laszlo.tumpech@proton.me>
2025-02-27 09:39:38 +0100

    Updated for course updates

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-def main(path_to_file):
+from stats import get_num_words
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path_to_file = sys.argv[1]
     file_contents = get_book_text(path_to_file)
     num_words = get_num_words(file_contents)
     character_count = get_char_count(file_contents)
-    
-    print(f'--- Begin report of {path_to_file} ---')
-    print(f"{num_words} words found in the document")
-    print()
-    print(gen_char_rep(character_count))
-    print('--- End report ---')
-    
+
+    print("============ BOOKBOT ============")
+    print(f'Analyzing book found at {path_to_file}...')
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    print(gen_char_rep(character_count),end='')
+    print('============= END ===============')
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
-
-def get_num_words(text):
-    return len(text.split())
 
 def get_char_count(text):
     result = {}
@@ -34,7 +39,7 @@ def gen_char_rep(char_count):
     char_count_dictlist = [{'char': x, 'num': char_count[x]} for x  in char_count]
     char_count_dictlist.sort(reverse=True, key=lambda x: x['num'])
     for char_count_dict in char_count_dictlist:
-        result += f"The '{char_count_dict['char']}' character was found {char_count_dict['num']} times\n"
+        result += f"{char_count_dict['char']}: {char_count_dict['num']}\n"
     return result
 
-main("books/frankenstein.txt")
+main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+
+def get_num_words(text):
+    return len(text.split())
